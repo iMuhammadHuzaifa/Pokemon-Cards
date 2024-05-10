404ec70ae730968ac6caf68182d1f960c530d2a7
iMuhammadHuzaifa <itshuzaifa09@gmail.com>
2024-05-11 03:37:58 +0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function handleMouse(tag) {
+    tag.src = "https://www.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SWSH4/SWSH4_EN_43.png"
+}
+
+function hoverMouse(tag) {
+    tag.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuCwHHN2FnRN3P6h9hty8XsM0XrNFRfFlxNS4sYHL2uA&s"
+}
+
+function charizard(tag) {
+    tag.src = "https://www.cardsbybrammers.com.au/assets/full/swshbrst018.jpg?20220308161651"
+}
+
+function hoverMouse(tag) {
+    tag.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuCwHHN2FnRN3P6h9hty8XsM0XrNFRfFlxNS4sYHL2uA&s"
+}
+
+function tryanitar(tag) {
+    tag.src = "https://vintage-action-figures.com/cdn/shop/files/SV03_EN_211-2x.jpg?v=1693317224"
+}
+
+function pikachu(tag) {
+    tag.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuCwHHN2FnRN3P6h9hty8XsM0XrNFRfFlxNS4sYHL2uA&s"
+}
+
+function mewtwo(tag) {
+    tag.src = "https://www.trainerhub.net.au/assets/full/GO-030.png?20230620180345"
+}
+
+function pikachu(tag) {
+    tag.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuCwHHN2FnRN3P6h9hty8XsM0XrNFRfFlxNS4sYHL2uA&s"
+}
